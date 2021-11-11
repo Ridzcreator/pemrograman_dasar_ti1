037a0b9192fada828b027ca208ebebbfd8a34a6d
NRidz <35356539+Ridzcreator@users.noreply.github.com>
2021-11-11 09:33:54 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Form </title>
+</head>
+<body>
+<h1 style ="color:blue;">Login</h1>
+<form>
+  <label for="fname">First name:</label><br>
+  <input type="text" id="fname" name="fname"><br>
+  <label for="lname">Last name:</label><br>
+  <input type="text" id="lname" name="lname">
+</form> 
+  <button class="button">Submit</button>
+
+</body>
+</html>
+
